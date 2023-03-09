d5e3ae9ddb6f1f5ebdaa4f47cd9ba239e4acbce7
Samuel Cupp <scupp1@my.apsu.edu>
2023-03-08 17:42:15 -0800

    Forgot to add new function file

---
+++ GRHayL/GRHayL_Core/return_stress_energy.c
@@ -0,0 +1,29 @@
+#include "GRHayL.h"
+
+/* Function    : return_stress_energy()
+ * Description : unpacks stress_energy struct into variables
+ *
+ * Inputs      : Tmunu          - stress_energy struct to be unpacked
+ *
+ * Outputs     : Tij            - pointer to variable for (i,j) component
+ *                                of T^\mu\nu
+ */
+
+void return_stress_energy(
+      const stress_energy *restrict Tmunu,
+      double *restrict Ttt, double *restrict Ttx, double *restrict Tty,
+      double *restrict Ttz, double *restrict Txx, double *restrict Txy,
+      double *restrict Txz, double *restrict Tyy, double *restrict Tyz,
+      double *restrict Tzz) {
+
+  *Ttt = Tmunu->Ttt;
+  *Ttx = Tmunu->Ttx;
+  *Tty = Tmunu->Tty;
+  *Ttz = Tmunu->Ttz;
+  *Txx = Tmunu->Txx;
+  *Txy = Tmunu->Txy;
+  *Txz = Tmunu->Txz;
+  *Tyy = Tmunu->Tyy;
+  *Tyz = Tmunu->Tyz;
+  *Tzz = Tmunu->Tzz;
+}
