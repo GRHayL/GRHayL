717af607cf7b5de26e34d091ff51e9ef394665bb
Samuel Cupp <scupp1@my.apsu.edu>
2023-04-12 14:14:27 -0700

    apply_inequality_fixes: code cleanup

--- GRHayL/Con2Prim/apply_inequality_fixes.c
+++ GRHayL/Con2Prim/apply_inequality_fixes.c
@@ -55,51 +55,29 @@ void apply_inequality_fixes(
   double hatBbardotS = BbardotS/Bbar;
   if (Bbar<1.e-300) hatBbardotS = 0.0;
 
-  // Limit hatBbardotS
-  //double max_gammav = 100.0;
-  //double rhob_max = CONSERVS[RHOSTAR]/METRIC_LAP_PSI4[PSI6];
-  //double hmax = 1.0 + 2.0*rhob_max;
-  //double abs_hatBbardotS_max = sqrt(SQR(max_gammav)-1.0)*CONSERVS[RHOSTAR]*hmax;
-  //if (fabs(hatBbardotS) > abs_hatBbardotS_max) {
-  //   double fac_reduce = abs_hatBbardotS_max/fabs(hatBbardotS);
-  //   double hatBbardotS_max = hatBbardotS*fac_reduce;
-  //   double Bbar_inv = 1.0/Bbar;
-  //   double hat_Bbar_x = Bbar_x*Bbar_inv;
-  //   double hat_Bbar_y = Bbar_y*Bbar_inv;
-  //   double hat_Bbar_z = Bbar_z*Bbar_inv;
-  //   double sub_fact = hatBbardotS_max - hatBbardotS;
-  //   CONSERVS[STILDEX] += sub_fact*hat_Bbar_x;
-  //   CONSERVS[STILDEY] += sub_fact*hat_Bbar_y;
-  //   CONSERVS[STILDEZ] += sub_fact*hat_Bbar_z;
-  //   hatBbardotS = hatBbardotS_max;
-  //   BbardotS *= fac_reduce;
-  //   CONSERVS[STILDEX] = CONSERVS[STILDEX]; CONSERVS[STILDEY] = CONSERVS[STILDEY]; CONSERVS[STILDEZ] = CONSERVS[STILDEZ];
-  //}
-
   const double sdots= metric->adm_gupxx*SQR(cons->S_x)+metric->adm_gupyy*SQR(cons->S_y)+metric->adm_gupzz*SQR(cons->S_z)+2.0*
     (metric->adm_gupxy*cons->S_x*cons->S_y+metric->adm_gupxz*cons->S_x*cons->S_z+metric->adm_gupyz*cons->S_y*cons->S_z);
 
   const double Wm = sqrt(SQR(hatBbardotS)+ SQR(cons->rho))/metric->psi6;
   const double Sm2 = (SQR(Wm)*sdots + SQR(BbardotS)*(Bbar2+2.0*Wm))/SQR(Wm+Bbar2);
   const double Wmin = sqrt(Sm2 + SQR(cons->rho))/metric->psi6;
-  const double sdots_fluid_max = sdots;
+  const double half_psi6_Bbar2 = 0.5*metric->psi6*Bbar2;
+  const double tau_fluid_term3 = (Bbar2*sdots - SQR(BbardotS))*0.5/(metric->psi6*SQR(Wmin+Bbar2));
 
   //tau fix, applicable when B==0 and B!=0:
-  if(cons->tau < 0.5*metric->psi6*Bbar2) {
-    cons->tau = eos->tau_atm+0.5*metric->psi6*Bbar2;
+  if(cons->tau < half_psi6_Bbar2) {
+    cons->tau = eos->tau_atm + half_psi6_Bbar2;
     diagnostics->failure_checker+=1000000;
   }
 
-  double tau_fluid_min = cons->tau - 0.5*metric->psi6*Bbar2 - (Bbar2*sdots - SQR(BbardotS))*0.5/(metric->psi6*SQR(Wmin+Bbar2));
+  double tau_fluid_min = cons->tau - half_psi6_Bbar2 - tau_fluid_term3;
 
   //Apply Stilde fix when B==0.
   if(check_B_small*check_B_small < eos->press_atm*1e-32) {
-    const double rhot = cons->tau*(cons->tau+2.0*cons->rho);
-    const double safetyfactor = 0.999999;
-    //if(METRIC_LAP_PSI4[PSI6]>Psi6threshold) safetyfactor=0.99;
+    const double rhot = 0.999999*cons->tau*(cons->tau+2.0*cons->rho);
 
-    if(sdots > safetyfactor*rhot) {
-      const double rfactm1 = sqrt((safetyfactor*rhot)/sdots);
+    if(sdots > rhot) {
+      const double rfactm1 = sqrt(rhot/sdots);
       cons->S_x*=rfactm1;
       cons->S_y*=rfactm1;
       cons->S_z*=rfactm1;
@@ -109,15 +87,13 @@ void apply_inequality_fixes(
   } else if(metric->psi6>params->psi6threshold) {
     if (tau_fluid_min < eos->tau_atm*1.001) {
       tau_fluid_min = eos->tau_atm*1.001;
-      cons->tau = tau_fluid_min + 0.5*metric->psi6*Bbar2 + (Bbar2*sdots - SQR(BbardotS))*0.5/(metric->psi6*SQR(Wmin+Bbar2));
+      cons->tau = tau_fluid_min + half_psi6_Bbar2 + tau_fluid_term3;
     }
 
-    const double LHS = tau_fluid_min*(tau_fluid_min+2.0*cons->rho);
-    const double RHS = sdots_fluid_max;
+    const double rhot = 0.999999*tau_fluid_min*(tau_fluid_min+2.0*cons->rho);
 
-    const double safetyfactor = 0.999999;
-    if(safetyfactor*LHS < RHS) {
-      const double rfactm1 = sqrt((safetyfactor*LHS)/RHS);
+    if(sdots > rhot) {
+      const double rfactm1 = sqrt(rhot/sdots);
       cons->S_x*=rfactm1;
       cons->S_y*=rfactm1;
       cons->S_z*=rfactm1;
