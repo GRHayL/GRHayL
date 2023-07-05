29a323d4494b1c54741e42135683d4ac7765c28e
Samuel Cupp <scupp1@my.apsu.edu>
2023-07-05 15:57:41 -0700

    GRHayLET repo contains all thorns other than GRHayLib

--- implementations/GRHayLMHD/src/sync.c
+++
@@ -1,4 +0,0 @@
-#include "GRHayLMHD.h"
-
-void GRHayLMHD_sync(CCTK_ARGUMENTS) {
-}
--- implementations/TestDataID/src/TestDataID.h
+++
@@ -1,9 +0,0 @@
-#ifndef TESTDATAID_H_
-#define TESTDATAID_H_
-
-#include "cctk.h"
-#include "cctk_Parameters.h"
-#include "cctk_Arguments.h"
-#include "GRHayLib.h"
-
-#endif // TESTDATAID_H_
