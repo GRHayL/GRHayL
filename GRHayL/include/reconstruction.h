63b2995cebf33e9c26cbe2f57351db6eb82aaef5
Terrence Pierre Jacques <terrencepierrej@gmail.com>
2024-07-29 15:39:04 -0700

    Fixed comments in make.code.defn in Reconstruction + added weno5 reconstruction, adapted from nubhlight

--- GRHayL/include/reconstruction.h
+++ GRHayL/include/reconstruction.h
@@ -92,6 +92,11 @@ void ghl_superbee_reconstruction(
       double *restrict Ur,
       double *restrict Ul);
 
+void ghl_weno5_reconstruction(
+      const double U[5],
+      double *restrict Ur,
+      double *restrict Ul);
+
 #ifdef __cplusplus
 }
 #endif
