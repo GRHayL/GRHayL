a1da2c7e75ddc217e1480837952193eef29b5640
Terrence Pierre Jacques <terrencepierrej@gmail.com>
2024-09-18 13:18:13 -0700

    Changing weno5 reconstruction to wenoz, which proves to be more accurate

--- Unit_Tests/data_gen/unit_test_data_WENOZ_reconstruction.c
+++ Unit_Tests/data_gen/unit_test_data_WENOZ_reconstruction.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
     var[index] = randf(1e-14, 1e-1);
   }
 
-  FILE* outfile = fopen_with_check("WENO5_reconstruction_input.bin", "wb");
+  FILE* outfile = fopen_with_check("WENOZ_reconstruction_input.bin", "wb");
   fwrite(&arraylength, sizeof(int), 1, outfile);
   fwrite(var, sizeof(double), arraylength, outfile);
   fclose(outfile);
@@ -27,18 +27,18 @@ int main(int argc, char **argv) {
     varpert[index] = var[index] * (1 + randf(-1.0,1.0)*1.0e-14);
   }
 
-  outfile = fopen_with_check("WENO5_reconstruction_output.bin", "wb");
-  FILE *outpert = fopen("WENO5_reconstruction_output_pert.bin", "wb");
+  outfile = fopen_with_check("WENOZ_reconstruction_output.bin", "wb");
+  FILE *outpert = fopen("WENOZ_reconstruction_output_pert.bin", "wb");
 
   for(int index=NGHOSTS; index<arraylength-2; index++) {
-    ghl_weno5_reconstruction(&var[index-3], &var_r[index], &var_l[index]);
+    ghl_wenoz_reconstruction(&var[index-3], &var_r[index], &var_l[index]);
   }
 
   fwrite(var_r, sizeof(double), arraylength, outfile);
   fwrite(var_l, sizeof(double), arraylength, outfile);
 
   for(int index=NGHOSTS; index<arraylength-2; index++) {
-    ghl_weno5_reconstruction(&varpert[index-3], &var_r[index], &var_l[index]);
+    ghl_wenoz_reconstruction(&varpert[index-3], &var_r[index], &var_l[index]);
   }
   
   fwrite(var_r, sizeof(double), arraylength, outpert);
