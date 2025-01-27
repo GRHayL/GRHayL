b1f7b27f15f9a34c15c9337e7a3f7d1aab79a5c4
Leo Werneck <wernecklr@gmail.com>
2025-01-27 10:33:52 -0800

    compute_number_density.c: fixed initialization bug

--- GRHayL/Radiation/M1/compute_number_density.c
+++ GRHayL/Radiation/M1/compute_number_density.c
@@ -4,43 +4,44 @@
 // Eq (21)
 // f^a = u^a + H^a/J
 void assemble_fnu(
-        const ghl_ADM_aux_quantities *adm_aux,
-        const double *u4U,
-        const double J,
-        ghl_radiation_flux_vector const * H4,
-        ghl_radiation_con_flux_vector * fnu4) {
-  double H4U[4];
-  for (int a = 0; a < 4; a++){
-    for (int b = 0; b < 4; b++){
+      const ghl_ADM_aux_quantities *adm_aux,
+      const double *u4U,
+      const double J,
+      const ghl_radiation_flux_vector *H4,
+      ghl_radiation_con_flux_vector *fnu4) {
+
+  double H4U[4] = { 0, 0, 0, 0 };
+  for(int a = 0; a < 4; a++) {
+    for(int b = 0; b < 4; b++) {
       H4U[a] += adm_aux->g4UU[a][b] * H4->D[b];
     }
   }
-  for (int a = 0; a < 4; a++) {
-      fnu4->U[a] = u4U[a] + H4U[a]/J;
+  for(int a = 0; a < 4; a++) {
+    fnu4->U[a] = u4U[a] + H4U[a] / J;
   }
 }
 
 // TODO: rad_E_floor rad_eps are param
 // Eq (24) -> Gamma = W(E/J)(1-f.v)
 double compute_Gamma(
-        const double W,
-        const double *v4U,
-        const double J,
-        const double E,
-        const double rad_E_floor,
-        const double rad_eps,
-        ghl_radiation_flux_vector const * F4) {
-          
-    if (E > rad_E_floor && J > rad_E_floor) {
-        double f_dot_v;
-        double f_dot_v_sum = 0;
-        for (int a = 0; a < 4; a++){
-          f_dot_v_sum += F4->D[a] * v4U[a];
-        }
-        f_dot_v = fmin(f_dot_v_sum, 1 - rad_eps);
-        return W*(E/J)*(1 - f_dot_v);
+      const double W,
+      const double *v4U,
+      const double J,
+      const double E,
+      const double rad_E_floor,
+      const double rad_eps,
+      const ghl_radiation_flux_vector *F4) {
+
+  if(E > rad_E_floor && J > rad_E_floor) {
+    double f_dot_v;
+    double f_dot_v_sum = 0;
+    for(int a = 0; a < 4; a++) {
+      f_dot_v_sum += F4->D[a] * v4U[a];
     }
-    else {
-        return 1;
-    }
+    f_dot_v = fmin(f_dot_v_sum, 1 - rad_eps);
+    return W * (E / J) * (1 - f_dot_v);
+  }
+  else {
+    return 1;
+  }
 }
