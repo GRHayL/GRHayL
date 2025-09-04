1533091b890db94a713a7ec2d276b57ac56f0a6f
Leo Werneck <wernecklr@gmail.com>
2025-09-03 18:28:29 -0700

    GRHayL/Con2Prim/*/Palenzuela1D/*_Palenzuela1D.c: replaced duplicated code with the appropriate auxiliary function

--- GRHayL/Con2Prim/Hybrid/Palenzuela1D/hybrid_Palenzuela1D.c
+++ GRHayL/Con2Prim/Hybrid/Palenzuela1D/hybrid_Palenzuela1D.c
@@ -67,31 +67,10 @@ ghl_error_codes_t ghl_hybrid_Palenzuela1D(
       ghl_primitive_quantities *restrict prims,
       ghl_con2prim_diagnostics *restrict diagnostics) {
 
-  // Step 1: Compute S^{2} = gamma^{ij}S_{i}S_{j}
-  double SD[3] = {cons_undens->SD[0], cons_undens->SD[1], cons_undens->SD[2]};
-  double S_squared = ghl_compute_vec2_from_vec3D(ADM_metric->gammaUU, SD);
+  double SU[3], B_squared, S_squared, BdotS;
+  compute_SU_Bsq_Ssq_BdotS(ADM_metric, cons_undens, prims, SU, &B_squared, &S_squared, &BdotS);
 
-  // Step 2: Enforce ceiling on S^{2} (Eq. A5 of [1])
-  // Step 2.1: Compute maximum allowed value for S^{2}
-  const double S_squared_max = SQR(cons_undens->tau + cons_undens->rho);
-  if(S_squared > S_squared_max) {
-    // Step 2.2: Rescale S_{i}
-    const double rescale_factor = sqrt(0.9999*S_squared_max/S_squared);
-    for(int i=0;i<3;i++)
-      SD[i] *= rescale_factor;
-
-    // Step 2.3: Recompute S^{2}
-    S_squared = ghl_compute_vec2_from_vec3D(ADM_metric->gammaUU, SD);
-  }
-
-  // Step 3: Compute B^{2} = gamma_{ij}B^{i}B^{j}
-  const double B_squared = ghl_compute_vec2_from_vec3D(ADM_metric->gammaDD, prims->BU);
-
-  // Step 4: Compute B.S = B^{i}S_{i}
-  double BdotS = 0.0;
-  for(int i=0;i<3;i++) BdotS += prims->BU[i]*SD[i];
-
-  // Step 5: Set specific quantities for this routine (Eq. A7 of [1])
+  // Set specific quantities for this routine (Eq. A7 of [1])
   fparams_struct fparams;
   const double invD           = 1.0/cons_undens->rho;
   fparams.compute_rho_P_eps_W = compute_rho_P_eps_W;
@@ -100,44 +79,36 @@ ghl_error_codes_t ghl_hybrid_Palenzuela1D(
   fparams.s                   = B_squared * invD;
   fparams.t                   = BdotS/pow(cons_undens->rho, 1.5);
 
-  // Step 6: Bracket x (Eq. A8 of [1])
+  // Bracket x (Eq. A8 of [1])
   double xlow = 1 + fparams.q - fparams.s;
   double xup  = 2*(1 + fparams.q) - fparams.s;
 
-  // Step 7: Call the main function and perform the con2prim
+  // Call the main function and perform the con2prim
   roots_params rparams;
   rparams.tol = 1e-15;
   rparams.max_iters = 300;
-  // ghl_toms748(froot, &fparams, xlow, xup, &rparams);
   ghl_error_codes_t error = ghl_brent(froot, params, eos, cons_undens, &fparams, prims, xlow, xup, &rparams);
   if(error)
     return error;
 
   diagnostics->n_iter = rparams.n_iters;
 
-  // Step 8: Set core primitives using the EOS and the root
+  // Set core primitives using the EOS and the root
   double x = rparams.root;
   double W;
   compute_rho_P_eps_W(x, params, eos, cons_undens, &fparams, prims, &W);
 
-  // Step 9: Compute auxiliary quantities to obtain the velocities using
-  //          Eq. (24) in [2]. Note, however, that GRHayL expects the velocity
-  //          tilde(u)^{i} := W v^{i},
-  // Step 9.a: Compute S^{i}
-  double SU[3];
-  ghl_raise_lower_vector_3D(ADM_metric->gammaUU, SD, SU);
-
-  // Step 9.b: Set Z
+  // Set Z
   const double Z = x*prims->rho*W;
 
-  // Step 9.c: Compute utilde^{i}
+  // Compute utilde^{i}
   double utildeU[3] = {
     W*(SU[0] + BdotS*prims->BU[0]/Z)/(Z+B_squared),
     W*(SU[1] + BdotS*prims->BU[1]/Z)/(Z+B_squared),
     W*(SU[2] + BdotS*prims->BU[2]/Z)/(Z+B_squared)
   };
 
-  // Step 9.d: Set prims struct
+  // Set prims struct
   diagnostics->speed_limited = ghl_limit_utilde_and_compute_v(params, ADM_metric, utildeU, prims);
   prims->entropy = ghl_hybrid_compute_entropy_function(eos, prims->rho, prims->press);
 
