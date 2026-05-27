2266568564d0f4c904e1ea95ed3cc02729f6693b
Leo Werneck <wernecklr@gmail.com>
2026-05-27 09:55:38 -0700

    GRHayL/EOS/Tabulated/*hdf5*: new HDF5 helper functions

---
+++ GRHayL/EOS/Tabulated/NRPyEOS_hdf5_helpers.c
@@ -0,0 +1,66 @@
+#include "NRPyEOS_hdf5_helpers.h"
+#include "ghl_io.h"
+
+void *
+NRPyEOS_hdf5_read_dataset(hid_t file_id, ghl_hdf5_t dtype, const char *dataset_name) {
+  const hid_t dataset_types[2] = { H5T_NATIVE_INT, H5T_NATIVE_DOUBLE };
+  const size_t dtype_size[2] = { sizeof(int), sizeof(double) };
+
+  const hid_t hdf5_dtype = dataset_types[dtype];
+
+  // Open the dataset
+  hid_t dataset_id = H5Dopen(file_id, dataset_name, H5P_DEFAULT);
+  if(dataset_id < 0) {
+    ghl_error("Dataset '%s' not found.\n", dataset_name);
+  }
+
+  // Get the dataspace and dimensions
+  hid_t dataspace_id = H5Dget_space(dataset_id);
+
+  int ndims = H5Sget_simple_extent_ndims(dataspace_id);
+  if(ndims < 1) {
+    H5Sclose(dataspace_id);
+    H5Dclose(dataset_id);
+    ghl_error("Invalid dimension for dataset '%s' (%d)\n", dataset_name, ndims);
+  }
+
+  hsize_t dims[ndims];
+  H5Sget_simple_extent_dims(dataspace_id, dims, NULL);
+
+  // Allocate memory for the 3D array
+  size_t total_size = 1;
+  for(int i = 0; i < ndims; i++) {
+    total_size *= dims[i];
+  }
+
+  // We don't use malloc_or_error so we can close the HDF5 file first.
+  void *array = malloc(total_size * dtype_size[dtype]);
+  if(!array) {
+    H5Sclose(dataspace_id);
+    H5Dclose(dataset_id);
+    ghl_error("Memory allocation failed for dataset '%s'.\n", dataset_name);
+  }
+
+  // Read the 3D array
+  herr_t status = H5Dread(dataset_id, hdf5_dtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, array);
+  if(status < 0) {
+
+    H5Sclose(dataspace_id);
+    H5Dclose(dataset_id);
+    ghl_error("Problem reading dataset '%s'.\n", dataset_name);
+  }
+
+  // Close the dataspace and dataset
+  H5Sclose(dataspace_id);
+  H5Dclose(dataset_id);
+
+  return array;
+}
+
+int *NRPyEOS_hdf5_read_int_dataset(hid_t file_id, const char *dataset_name) {
+  return (int *)NRPyEOS_hdf5_read_dataset(file_id, GHL_HDF5_INT, dataset_name);
+}
+
+double *NRPyEOS_hdf5_read_double_dataset(hid_t file_id, const char *dataset_name) {
+  return (double *)NRPyEOS_hdf5_read_dataset(file_id, GHL_HDF5_DOUBLE, dataset_name);
+}
---
+++ GRHayL/EOS/Tabulated/NRPyEOS_hdf5_helpers.h
@@ -0,0 +1,37 @@
+/**
+ * @file NRPyEOS_hdf5_helpers.h
+ * @author Leo Werneck
+ *
+ * @brief Helper functions for reading and writing datasets in HDF5 files.
+ */
+
+#ifndef NRPYEOS_HDF5_HELPERS_H
+#define NRPYEOS_HDF5_HELPERS_H
+
+#include <hdf5.h>
+
+/*
+ * @brief Convenience enumeration to index HDF5 data types.
+ */
+typedef enum {
+  GHL_HDF5_INT,
+  GHL_HDF5_DOUBLE,
+} ghl_hdf5_t;
+
+/**
+ * @brief Reads an HDF5 dataset from a file.
+ *
+ * @param file_id The HDF5 file identifier.
+ * @param dtype The data type of the dataset (I32 or F64).
+ * @param dataset_name The name of the dataset to read.
+ *
+ * @return A pointer to the allocated memory containing the dataset data.
+ *         The caller is responsible for freeing this memory. Returns NULL on error.
+ */
+void *NRPyEOS_hdf5_read_dataset(hid_t file_id, ghl_hdf5_t dtype, const char *dataset_name);
+
+// Convenience functions
+int *NRPyEOS_hdf5_read_int_dataset(hid_t file_id, const char *dataset_name);
+double *NRPyEOS_hdf5_read_double_dataset(hid_t file_id, const char *dataset_name);
+
+#endif // NRPYEOS_HDF5_HELPERS_H
