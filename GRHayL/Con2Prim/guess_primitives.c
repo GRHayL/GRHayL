38585544edb2de119e1f23519808b682705a63de
Leo Werneck <wernecklr@gmail.com>
2026-06-24 14:40:17 -0700

    GRHayL/Con2Prim/guess_primitives.c: updated doxygen comments

--- GRHayL/Con2Prim/guess_primitives.c
+++ GRHayL/Con2Prim/guess_primitives.c
@@ -19,10 +19,10 @@
  * \f]
  *
  * This choice sets the transport/utilde velocity \f$ v^i+\beta^i \f$ to zero;
- * it is only an initial guess for the Con2Prim solve. Also, the quantity
- * \f$ T_\mathrm{max} \f$ uses ghl_eos_parameters::T_max. Finally, for hybrid or simple EOS,
- * we set the pressure and specific internal energy \f$ \epsilon \f$ to the
- * cold values.
+ * it is only an initial guess for the Con2Prim solve. We set the pressure and
+ * specific internal energy \f$ \epsilon \f$ to the cold values.
+ *
+ * @param[in] params pointer to ghl_parameters struct
  *
  * @param[in] eos pointer to ghl_eos_parameters struct
  *
@@ -54,6 +54,30 @@ static void ghl_guess_primitives_hybrid_simple(
   ghl_hybrid_compute_P_cold_and_eps_cold(eos, prims->rho, &prims->press, &prims->eps);
 }
 
+/**
+ * @ingroup Con2Prim
+ * @brief Computes an initial guess for the @ref Con2Prim solvers
+ *
+ * @details
+ * This function sets a default initial guess for tabulated EOSs. It can be
+ * used whenever a better guess is not available. It sets primitives following
+ * the primitive-recovery strategy of the Palenzuela et al. routine, as outlined
+ * in Siegel et al. (2018; https://arxiv.org/pdf/1712.07538).
+ *
+ * The only required guess is the temperature. We use \f$ T = T_\mathrm{max} \f$,
+ * which uses ghl_eos_parameters::T_max.
+ *
+ * @param[in] params pointer to ghl_parameters struct
+ *
+ * @param[in] eos pointer to ghl_eos_parameters struct
+ *
+ * @param[in] metric_adm pointer to ghl_metric_quantities struct with ADM metric
+ *
+ * @param[in] cons_undens pointer to ghl_conservative_quantities struct with
+ *                        **undensitized** conservative variables
+ *
+ * @param[out] prims pointer to ghl_primitive_quantities containing the initial guess
+ */
 static void ghl_guess_primitives_tabulated(
       const ghl_parameters *restrict params,
       const ghl_eos_parameters *restrict eos,
@@ -113,6 +137,21 @@ static void ghl_guess_primitives_tabulated(
   ghl_limit_utilde_and_compute_v(params, metric_adm, utildeU, prims);
 }
 
+/**
+ * @ingroup Con2Prim
+ * @brief Computes an initial guess for the @ref Con2Prim solvers
+ *
+ * @param[in] params pointer to ghl_parameters struct
+ *
+ * @param[in] eos pointer to ghl_eos_parameters struct
+ *
+ * @param[in] metric_adm pointer to ghl_metric_quantities struct with ADM metric
+ *
+ * @param[in] cons_undens pointer to ghl_conservative_quantities struct with
+ *                        **undensitized** conservative variables
+ *
+ * @param[out] prims pointer to ghl_primitive_quantities containing the initial guess
+ */
 void ghl_guess_primitives(
       const ghl_parameters *restrict params,
       const ghl_eos_parameters *restrict eos,
